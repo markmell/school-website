2c38bd73395cf764cf67608bb279c20ca92f8c6c
Mark Emeliano <meloberita@gmail.com>
2026-05-15 12:27:44 +0800

    updated school website

--- src/firebase.ts
+++ src/firebase.ts
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDuagICGs4fzfOscJmrE1HQmfGHIJcOibQ",
-  authDomain: "uep-prmc.firebaseapp.com",
-  projectId: "uep-prmc",
-  storageBucket: "uep-prmc.firebasestorage.app",
-  messagingSenderId: "200143474474",
-  appId: "1:200143474474:web:ebcd582ab160840b9a5853"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
